41cc7162119801f28aef72957821f131a3788b7f
JoonyWoony <80926013+JoonyWoony@users.noreply.github.com>
2023-03-23 19:04:08 +1100

    advanced label

---
+++ src/advanced_labels.py
@@ -0,0 +1,2 @@
+label = Label(icon='fa:chevron-circle-right', text=f'WARNING: *Password cannot contain white spaces.', bold=True, background=green)
+# Label Icon, #Label Text, #Label bold boolean Background = green
