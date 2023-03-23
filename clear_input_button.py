e14699e7a017125149476c74dd785410b725d12b
Mashiro Shiina <80926013+JoonyWoony@users.noreply.github.com>
2023-03-23 12:08:37 +1100

    Clearing inputs

---
+++ clear_input_button.py
@@ -0,0 +1,6 @@
+def clear_inputs(self):
+    self.name_box.text = ""
+    # change name_box to the text box that you want to clear
+def submit_button_click(self, **event_args):
+    self.clear_inputs()
+    #clears the input in the textbox that you specified
