1bb2943e40395891ea56bd59d298eaa9d90262fd
Mashiro Shiina <80926013+JoonyWoony@users.noreply.github.com>
2023-03-24 11:38:17 +1100

    advanced database adding rows

---
+++ src/server/database_adding_rows_advanced.py
@@ -0,0 +1,4 @@
+# from anvil.tables import app tables
+# seek advanced documentation here : {https://anvil.works/docs/data-tables}
+row = app_tables.database_name.add_row(Name='Jane Smith' , Age=10, Employ_position='Crew', Employed_At='Mcdonalds') #Name, Age [Int], Employ position [String] , Employed at [String]
+# feel free to modify to your own rows, and ensure to change database_name to your own database.
